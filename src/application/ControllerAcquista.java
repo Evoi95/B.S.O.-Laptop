d01c11a17a3d1f6d692ad3b859ba80f8d0f87062
ilciro <danielecinus10@gmail.com>
2020-11-25 16:08:38 +0400

    provo singleton libro

--- src/application/ControllerAcquista.java
+++ src/application/ControllerAcquista.java
@@ -1,10 +1,14 @@
 package application;
 
+import b.s.o.LibroSingleton;
+
 public class ControllerAcquista {
 	
 	public void totale()
 	{
 		//calcolo qui il toatale
 	}
-
+	LibroSingleton lS=LibroSingleton.getIstance();
+	
+	//lS.getLibro();
 }
---
+++ src/b/s/o/LibroSingleton.java
@@ -0,0 +1,22 @@
+package b.s.o;
+
+public class LibroSingleton {
+	
+	private static LibroSingleton instance=new LibroSingleton();
+	
+	private LibroSingleton()
+	{
+		
+	}
+	
+	public static  LibroSingleton getIstance()
+	{
+		return instance;
+	}
+	
+	public void getLibro()
+	{
+		System.out.println("istanza libro");
+	}
+
+}
